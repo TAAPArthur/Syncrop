2084c89e4b2183c21af7bd1651e9bfd1f7efd823
Arthur Williams <taaparthur@gmail.com>
2015-09-27 11:21:21 -0500

    Moved to separate class

---
+++ Syncrop/SyncropDaemon/src/syncrop/RecursiveDeletionFileVisitor.java
@@ -0,0 +1,26 @@
+package syncrop;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class RecursiveDeletionFileVisitor extends SimpleFileVisitor<Path>{
+	
+	@Override
+	public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+		dir.toFile().delete();
+		return FileVisitResult.CONTINUE;
+	}
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		file.toFile().delete();
+		return FileVisitResult.CONTINUE;
+	}
+	@Override
+	public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+		return FileVisitResult.SKIP_SUBTREE;
+	}	
+
+}
